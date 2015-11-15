5dd89bc1075f1f2a79a1b08dea80e38f6f7244fb
J. B <jubi721@gmail.com>
2015-11-15 19:32:35 +0100

    Create master.py, tree.py, README.md

---
+++ master.py
@@ -0,0 +1,53 @@
+from tree import * # Funktionsnamen ueberschneiden sich nicht, da selbst definiert
+
+class HuffCode:
+    
+    def __init__ (self):
+        """
+        pre: None
+        post: HuffCode initialized
+        """
+        self.__text     = None # Text, der eingelesen wird bei self.write
+        self.__freqlist = None # Liste mit Haeufigkeiten
+        self.__codeTree = None # Hauptbaum mit Haeufigkeiten pro Zeichen
+        self.__bitSeq   = None # Zeichenfolge mit codierten Bits; Code für Datei
+        
+
+    def write (self, text, filename = "code.hfc"):
+        """
+        pre: text given
+        post: write huffcode-compressed text into file called filename
+              if no filename given file is called code.hfc
+        """
+        pass
+
+    def __getFrequencies (self, string):
+        """
+        pre: string given
+        post: returns list of type Tree with frequency for each character in string
+        """
+        pass
+
+    def __getCodeTree (self, freqlist):
+        """
+        pre: frequencies as list of type Tree given
+        post: returns Tree of frequencies
+        """
+        pass
+
+    def __getBitSequ (self, string, codeTree):
+        """
+        pre: user's string and code Tree given
+        post: returns bit sequence to write into file
+        """
+        pass
+
+    def __writeFile (self, bitSeq, codeTree, filename):
+        """
+        pre:  bit sequence bitSeq given
+              filename is same as in self.write
+              codeTree is same as in self.__getBitSeq
+        post: write file called filename using bit sequence
+        """
+        pass
+
---
+++ tree.py
@@ -0,0 +1,19 @@
+class Tree:
+    
+    def __init__ (self, freq, char = None):
+        """
+        pre:  frequency must be given
+        post: Tree is made with frequency freq and character char
+              if no char given, char is None (should appear in any root)
+        """
+        self.frequency = freq # Haeufigkeit
+        self.char      = char # Zeichen, für das die Haeufigkeit gilt
+        self.left      = None # linker Unterbaum
+        self.right     = None # rechter Unterbaum
+
+    def show (self):
+        """
+        pre:  Tree initialized (None)
+        post: returns tree as to show in HuffCode output file
+        """
+        pass
